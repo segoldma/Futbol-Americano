48358cd7e86e14e87918ee4b8a72c994c23c075b
Seth Goldman <goldmans@terpmail.umd.edu>
2018-08-08 15:13:28 -0400

    Lunch commit :hamburger:

---
+++ pbp acquisition.R
@@ -0,0 +1,14 @@
+# Load Pkg
+library(nflscrapR)
+
+# Query play-by-play data for past seasons
+system.time(pbp_2015 <- season_play_by_play(2015))
+system.time(pbp_2016 <- season_play_by_play(2016))
+system.time(pbp_2017 <- season_play_by_play(2017))
+
+# Save results locally as RDS
+saveRDS(pbp_2015, "pbp_2015")
+saveRDS(pbp_2016,"pbp_2016")
+saveRDS(pbp_2017, "pbp_2017")
+
+
