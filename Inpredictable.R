ee691aef1f1e406606ffc15386d78ca17185ffad
Seth Goldman <goldmans@terpmail.umd.edu>
2018-09-10 21:18:41 -0400

    Updated variable names

--- Inpredictable.R
+++ Inpredictable.R
@@ -1,5 +1,5 @@
 
-# Implied NFL Power Ratings Implied from Betting Market  ------------------
+# Implied NFL Power Ratings from Betting Market  ------------------
 # Mike Beouy's website, Inpredictable (http://inpredictable.com)
 
 library(rvest)
@@ -7,18 +7,18 @@ library(dplyr)
 library(stringr)
 
 # Query NFL Rankings
-inpredict <- read_html("http://stats.inpredictable.com/rankings/nfl.php") %>% 
+nfl_inpredictable <- read_html("http://stats.inpredictable.com/rankings/nfl.php") %>% 
   html_nodes("table") %>% 
   html_table(fill = TRUE) %>% 
   as.data.frame() %>%  # Drop Ranking Columns
   select(2,5,7,10,13,15,17,19)
 
 # Rename headers with data from row 2
-colnames(inpredict) <- as.character(unlist(inpredict[1,]))
+colnames(nfl_inpredictable) <- as.character(unlist(nfl_inpredictable[1,]))
 
 # Update column datatypes. Drop extra header row.
-inpredict <- inpredict %>% 
-  slice(2:nrow(inpredict)) %>% 
+nfl_inpredictable <- nfl_inpredictable %>% 
+  slice(2:nrow(nfl_inpredictable)) %>% 
   mutate("Team" = str_replace(`Team`,"&nbsp",""),
          "GPF" = as.numeric(GPF),
          "oGPF" = as.numeric(oGPF),
